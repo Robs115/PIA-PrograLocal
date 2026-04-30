f4dc8837cf9be53c8b7f83254d6ea9a1c66e4e92
Robs115 <RobertoEstradaSamdoval@hotmail.com>
2026-04-30 16:19:21 -0600

    Agregadas Graficas en reportes para ejemplificar

--- piaWinUI/Views/Reportes.xaml.cs
+++ piaWinUI/Views/Reportes.xaml.cs
@@ -12,20 +12,49 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace piaWinUI.Views
+namespace piaWinUI
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class Reportes : Page
     {
+        public ISeries[] VentasSeries { get; set; }
+        public ISeries[] TendenciaSeries { get; set; }
+        public ISeries[] ProductosSeries { get; set; }
+
         public Reportes()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            // 📊 BARRAS
+            VentasSeries = new ISeries[]
+            {
+                new ColumnSeries<double>
+                {
+                    Values = new double[] { 200, 450, 300, 600, 500 }
+                }
+            };
+
+            // 📈 LÍNEA
+            TendenciaSeries = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 100, 300, 250, 400, 550 }
+                }
+            };
+
+            // 🥧 PASTEL
+            ProductosSeries = new ISeries[]
+            {
+                new PieSeries<double> { Values = new double[] { 40 } },
+                new PieSeries<double> { Values = new double[] { 30 } },
+                new PieSeries<double> { Values = new double[] { 20 } }
+            };
+
+            // 🔥 ESTO ES LO QUE TE FALTABA
+            this.DataContext = this;
         }
     }
 }
